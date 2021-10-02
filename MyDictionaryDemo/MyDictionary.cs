d46b6dfc222fee2b662e3552b4f312c6cf228327
BuildTools <unconfigured@null.spigotmc.org>
2021-10-02 15:20:37 +0300

    Proje dosyası ekle.

---
+++ MyDictionaryDemo/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionaryDemo
+{
+    class MyDictionary<K,V>
+    {
+        K[] key; V[] value;
+        K[] tempKey; V[] tempValue;
+        
+        public MyDictionary()
+        {
+            key = new K[0];
+            value = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            tempKey = this.key;
+            tempValue = this.value;
+
+            this.key = new K[this.key.Length + 1];
+            this.value = new V[this.value.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++) {
+                this.key[i] = tempKey[i];
+            }
+            for (int i = 0; i < tempValue.Length; i++) {
+                this.value[i] = tempValue[i];
+            }
+            this.key[this.key.Length - 1] = key;
+            this.value[this.value.Length - 1] = value;
+        }
+
+        public int Length()
+        {
+            return this.key.Length;
+        }
+    }
+}
---
+++ MyDictionaryDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionaryDemo
+{
+    class Program
+    {
+        //My Dictionary Demo
+        static void Main(string[] args)
+        {
+            MyDictionary<String, int> vehicles = new MyDictionary<string, int>();
+            vehicles.Add("Sport Car", 5000);
+            vehicles.Add("Tractor", 2000);
+            vehicles.Add("Truck", 3000);
+            vehicles.Add("Helicopter", 10000);
+            vehicles.Add("Plane", 25000);
+
+            Console.WriteLine(vehicles.Length());
+        }
+    }
+}
